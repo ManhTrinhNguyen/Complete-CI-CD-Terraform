c5a566191353012593ec9d7c00dbb52d2a2a1ad3
Trinh Nguyen <trinhnguyen@Trinhs-MacBook-Pro.local>
2025-05-06 13:39:11 -0700

    Update

--- terraform/entry_script.sh
+++ terraform/entry_script.sh
@@ -10,3 +10,5 @@ sudo usermod -aG docker ec2-user
 sudo curl -SL "https://github.com/docker/compose/releases/download/v2.35.0/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Success" 
